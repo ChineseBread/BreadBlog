69111b6f1b245b2583f2ed861af266ebf2c378c7
Chinesebread <76460624+ChineseBread@users.noreply.github.com>
2022-04-15 20:12:55 +0800

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom";
+import App from './App';
+import './index.css';
+
+// createRoot(document.getElementById('root')).render(<App/>)
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
