f712ea0ef2db1802c77865f3d6ffa50e697e7e21
Chinesebread <76460624+ChineseBread@users.noreply.github.com>
2022-04-15 20:05:43 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import {lazy, useEffect,Suspense} from 'react';
+import {BrowserRouter} from "react-router-dom";
+import Particles from 'particlesjs/dist/particles.min'
+import Loading from "./components/Loading/Loading";
+import ParticleCanvas from "./components/BreadBlog/utilsComponents/ParticleCanvas";
+import './App.less';
+/**
+ * @version 1.0
+ * @description 基于react开发的博客管理系统
+ * @Author Chinesebread
+ */
+const Index = lazy(() => import('./components/BreadBlog/index'))
+function App() {
+    useEffect(() => {
+        Particles.init({
+            selector: '.background',
+            maxParticles: 70,
+            connectParticles:true,
+            minDistance: 200,
+            speed:0.3,
+            color:'#434343'
+        });
+    },[])
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <ParticleCanvas/>
+                {/*<Loading/>*/}
+                <Suspense fallback={<Loading/>} >
+                    <Index/>
+                </Suspense>
+            </div>
+        </BrowserRouter>
+
+    );
+}
+
+export default App;
