6f356c18c01ebfcd4c95d1ea45e07f35d114a294
Chinesebread <76460624+ChineseBread@users.noreply.github.com>
2022-04-19 20:35:10 +0800

    Update UserComponentsRouterCuard.jsx

--- src/router/UserComponentsRouterCuard.jsx
+++ src/router/UserComponentsRouterCuard.jsx
@@ -7,8 +7,6 @@ import UserOperationRequest from "../utils/RequestUtils/UserOperationRequest";
 export default function UserComponentsRouterCuard(props) {
 
 	const navigator = useNavigate()
-	// const [isPending,startTransition] = useTransition()
-	// const [startTransition,isPending] = useTransition()
 	const [pending,setPending] = useState(true)
 	useEffect( () => {
 		UserOperationRequest.beforeDoUserOperations().then(result => {
