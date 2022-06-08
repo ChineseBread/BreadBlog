a37c18fa2a7b0ae37acf0f135fb56b827ee7f6b1
Chinesebread <76460624+ChineseBread@users.noreply.github.com>
2022-06-08 18:43:32 +0800

    Delete src/components/BreadBlog/UserComponents/Article directory

--- src/components/BreadBlog/UserComponents/Article/update/index.d.ts
+++
@@ -1,9 +0,0 @@
-type UpdateArticleInfo = {
-    articleid:string,
-    sortname:string,
-    title:string,
-    content:string,
-    description:string,
-    ispublic:boolean,
-    tags:string[],
-}
